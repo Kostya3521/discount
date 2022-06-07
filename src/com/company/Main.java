c3b3511aaa6f92e436fedd778566794856e2d3e9
Kostya3521 <kostya.fommin.02@bk.ru>
2022-06-07 18:02:01 +0500

    version2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,20 @@ import java.util.Arrays;
 
 public class Main {
 
-    static public int[] decryptData( int[] price, int discount, int offset, int readLength) {
+    static public int[] decryptData(int[] price, int discount, int offset, int readLength) {
         int[] res = new int[readLength];
         double temp;
         int indx = 0;
         if ( (price.length != 0) & (0 <= offset) & (1 <= discount & discount <= 99)
-                & (readLength > 0 & readLength <= price.length) ){
+                & (readLength > 0 & readLength <= price.length - offset) ){
             for (int i = offset; i < offset + readLength; i++){
                 temp = (double) price[i] - ((double) price[i] * ((double)discount  / 100));
                 res[indx] = (int) Math.floor(temp) ;
                 indx++;
             }
+        }else {
+            System.out.println("Некоректные данные");
+            return null;
         }
         return res;
     }
@@ -24,7 +27,7 @@ public class Main {
         int[] price = {10, 20, 33, 40, 60};
         int discount = 50;
         int offset = 1;
-        int readLength = 3;
+        int readLength = 0;
         System.out.println(Arrays.toString(decryptData(price, discount, offset, readLength)));
     }
 
